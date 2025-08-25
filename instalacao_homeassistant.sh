5caaf09116917159e2fea727284b078a6710ab6b
PhamTz7 <150497421+PhamTz7@users.noreply.github.com>
2025-08-25 04:00:29 -0300

    Update instalacao_homeassistant.sh

--- instalacao_homeassistant.sh
+++ instalacao_homeassistant.sh
@@ -129,15 +129,11 @@ install_osagents() {
   echo ""
   echo "A instalar os agents..."
   echo ""
-#  wget https://github.com/home-assistant/os-agent/releases/download/1.4.1/os-agent_1.4.1_linux_aarch64.deb
-  wget https://github.com/home-assistant/os-agent/releases/download/1.5.1/os-agent_1.5.1_linux_aarch64.deb
-  sudo dpkg -i os-agent_1.5.1_linux_aarch64.deb
+  wget https://github.com/home-assistant/os-agent/releases/download/1.7.2/os-agent_1.7.2_linux_aarch64.deb
+  sudo dpkg -i os-agent_1.7.2_linux_aarch64.deb
   gdbus introspect --system --dest io.hass.os --object-path /io/hass/os
   systemctl status haos-agent --no-pager
 
-#  wget https://github.com/home-assistant/os-agent/releases/download/1.4.1/os-agent_1.4.1_linux_aarch64.deb
-#  sudo dpkg -i os-agent_1.4.1_linux_aarch64.deb
-#  gdbus introspect --system --dest io.hass.os --object-path /io/hass/os
 }
 
 # ------------------------------------------------------------------------------
@@ -147,10 +143,6 @@ install_hassio() {
   echo ""
   echo "A instalar o Home Assistant..."
   echo ""
-#  apt-get update
-#  apt-get install udisks2 wget -y
-#  wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
-#  sudo dpkg -i homeassistant-supervised.deb
 
   apt-get update
   apt-get install udisks2 wget -y
